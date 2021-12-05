9a54e88b497c3a6278e5c3c5e5c9f1a161b830f2
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-05 03:07:49 -0500

    added cleanup, handlesubmit

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function NewStudentContainer() {
+function NewStudentContainer(props) {
   const [firstname, setfname] = useState("");
   const [lastname, setlname] = useState("");
   const [campusId, setcid] = useState(null);
   const [redirect, isRedirect] = useState(false);
   const [redirectId, setrid] = useState(null);
+
+  //returning functions as a cleanup/ component will unmount
+  useEffect(() => {
+    return function cleanup() {
+      isRedirect(false);
+      setrid(null);
+    };
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let student = {
+      firstname: firstname,
+      lastname: lastname,
+      campusId: campusId,
+    };
+    let newStudent = await props.addStudent(student);
+    setfname("");
+    setlname("");
+    setcid(null);
+    isRedirect(true);
+    setrid(newStudent.id);
+  };
 
   return <div></div>;
 }
