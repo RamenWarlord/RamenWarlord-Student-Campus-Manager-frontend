1179feef42d3fa4c9c674f3f9c4772e7bc8398f5
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-05 00:21:14 -0500

    init new student state

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewStudentContainer() {
+  const [firstname, setfname] = useState("");
+  const [lastname, setlname] = useState("");
+  const [campusId, setcid] = useState(null);
+  const [redirect, isRedirect] = useState(false);
+  const [redirectId, setrid] = useState(null);
+
   return <div></div>;
 }
 
