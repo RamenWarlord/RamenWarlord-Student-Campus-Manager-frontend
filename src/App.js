e5664694f09636f57b0ca4d1eee2946f166f0d6b
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-05 15:10:44 -0500

    setting up test in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useSelector } from "react-redux";
-
+import { Switch, Route } from "react-router-dom";
+import NewStudentContainer from "./components/containers/NewStudentContainer";
 function App() {
   const state = useSelector((state) => state);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={NewStudentContainer} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
