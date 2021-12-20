a43427b6add45a1a459c0f9d77c33550587b8c73
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-20 02:49:21 -0500

    impl container

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -2,3 +2,4 @@ export { default as NewStudentContainer } from "./NewStudentContainer";
 export { default as HomeViewContainer } from "./HomeViewContainer";
 export { default as CampusListContainer } from "./CampusListContainer";
 export { default as CampusContainer } from "./CampusContainer";
+export { default as StudentsListContainer } from "./StudentsListContainer";
