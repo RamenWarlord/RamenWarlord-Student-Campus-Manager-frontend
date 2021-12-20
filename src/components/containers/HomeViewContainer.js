3f30578f44fa22af336c7ae6ae75073f06e85c8f
ramenwarlord <ivanyeung12345@gmail.com>
2021-12-19 23:39:19 -0500

    copied over homepage starter and modified it

---
+++ src/components/containers/HomeViewContainer.js
@@ -0,0 +1,7 @@
+import HomeView from "../views/HomeView";
+
+const HomeViewContainer = () => {
+  return <HomeView />;
+};
+
+export default HomeViewContainer;
--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,1 +1,2 @@
 export { default as NewStudentContainer } from "./NewStudentContainer";
+export { default as HomeViewContainer } from "./HomeViewContainer";
